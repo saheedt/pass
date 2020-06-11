61bcfeabad2352777befbe86ce4d673a9024f447
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-11 11:39:24 +0100

    - add abstarct CustomError class to act as base for all custom errors - modify errorHandler middleware to use abstract CustomError class to identify all custom errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,7 +1,13 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError} from './custom-errors';
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
   reason = 'Error connecting to database';
   constructor() {
-    super();
+    super('Error connecting to DB');
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
