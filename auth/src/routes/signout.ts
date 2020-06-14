8ac42c8578f1e3e772e51b7f8af07eecc5fb3540
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-14 14:21:29 +0100

    add signout route handler logic to un-auth user

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/api/v1/users/signout', (req: Request, res: Response)=> {
-  res.send('Hi there!!!!');
+  req.session = null;
+  res.status(200).send({});
 });
 
 export { router as signOutRouter };
