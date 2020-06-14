96bfd9e60022b06eace05bd8455da1273b2d2b41
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-14 14:39:50 +0100

    - add currentUser middleware for reusability - add currentUser middleware to currentuser route + delete duplicate logic from the route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,11 @@
 import express, { Request, Response } from 'express';
 
-import { JwtManager } from '../services/jwt-manager';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/v1/users/currentuser', (req: Request, res: Response)=> {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = JwtManager.verifyToken(req.session.jwt);
-    res.status(200).send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
-
+router.get('/api/v1/users/currentuser', currentUser, (req: Request, res: Response) => {
+  res.status(200).send({ currentUser: req.currentUser || null})
 });
 
 export { router as currentUserRouter };
