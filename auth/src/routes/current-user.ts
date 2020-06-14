c15359f2d6731e9956b644837e3c64478d69e171
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-14 14:16:34 +0100

    add currentuser route handler logic to verify user auth status

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express';
 
+import { JwtManager } from '../services/jwt-manager';
+
 const router = express.Router();
 
 router.get('/api/v1/users/currentuser', (req: Request, res: Response)=> {
-  res.send('Hi there!!!!');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = JwtManager.verifyToken(req.session.jwt);
+    res.status(200).send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
+
 });
 
 export { router as currentUserRouter };
