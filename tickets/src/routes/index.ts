c50b95070c92f61d2bc3c5132f45f1986ac65ac7
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-21 21:30:12 +0100

    add indexTicket router to fetch all tickets

---
+++ tickets/src/routes/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+import { Ticket } from '../db/models/ticket';
+
+const router = express.Router();
+
+router.get('/api/v1/tickets', async (req: Request, res: Response) => {
+  const tickets = await Ticket.find({});
+
+  res.status(200).send(tickets);
+});
+
+export { router as indexTicketRouter };
