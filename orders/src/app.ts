b6de68a2fa759d0c8165010852a57809c5868dc0
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-25 16:15:00 +0100

    - add docker image description file (Dockerfile) and .dockerignore - generate and add dependencies to package.json

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+import { errorHandler, RouteNotFoundError, currentUser } from '@saheedpass/common';
+
+const app = express();
+// tells express to trust proxy as
+// service is behind ngix ingress proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', (req: Request, res: Response) => {
+  throw new RouteNotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
