642e1e63820ed79615076dfa142a1f0ce0e9f5ec
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-26 23:12:17 +0100

    - implement route handler to fetch a single order - add tests to verify route handler to fetch an order works correctly

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@saheedpass/common';
+import { Order } from '../db/models/order';
 
 const router = express.Router();
 
-router.get('/api/v1/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/v1/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError('Order not found');
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
