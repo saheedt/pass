0ebbfc97353d6c5c6f7107069103cd6d616d8926
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-26 22:17:35 +0100

    - add implementation to fetch all user order - add test to verify route to fetch a user's orders work correctly

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../db/models/order';
+import { Ticket } from '../../db/models/ticket';
+import { getCookie } from '../../test/auth-helper';
+
+const buildTicket = async (title: string, price: number) => {
+  const ticket = Ticket.build({
+    title,
+    price
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+it('fetches orders for a user', async () => {
+  const ticketOne = await buildTicket('first', 20);
+  const ticketTwo = await buildTicket('second', 30);
+  const ticketThree = await buildTicket('third', 40);
+
+  const userOne = getCookie();
+  const userTwo = getCookie();
+
+  await request(app)
+    .post('/api/v1/orders')
+    .set('Cookie', userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+  
+  const { body: orderOne } = await request(app)
+    .post('/api/v1/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  
+  const { body: orderTwo } = await request(app)
+    .post('/api/v1/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+  
+  const response = await request(app)
+    .get('/api/v1/orders')
+    .set('Cookie', userTwo)
+    .expect(200);
+  
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@saheedpass/common';
+import { Order } from '../db/models/order';
 
 const router = express.Router();
 
-router.get('/api/v1/orders', async (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/v1/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id
+  }).populate('ticket');
+  
+  res.status(200).send(orders);
 });
 
 export { router as indexOrderRouter };
