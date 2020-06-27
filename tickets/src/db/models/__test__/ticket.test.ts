51e47611cffe2045433e76403f6f91b96bafb42e
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-06-27 22:03:26 +0100

    - add mongoose-update-if-current module to help with Optimistic Concurrency Control (OCC), aka data versioning - wireup mongoose-update-if-current with Ticket model - add test to test TicketDoc versioning

---
+++ tickets/src/db/models/__test__/ticket.test.ts
@@ -0,0 +1,31 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async (done) => {
+  const ticket = Ticket.build({
+    title: 'mov',
+    price: 12,
+    userId: 'khabbfaj'
+  })
+
+  await ticket.save();
+
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  firstInstance!.set({ price: 15 });
+  secondInstance!.set({ price: 20 });
+  
+  await firstInstance!.save();
+
+  // because of jest issues with typescript when using
+  // async function in a expect like so:
+  // expect(async ()=> ).toThrow(), we will be using a
+  // A TRY/CATCH trick to test this.
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error('Should not reach this point');
+});
