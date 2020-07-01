0e5431fb669590039b630de84bafc1b26fe1d200
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-07-01 13:22:49 +0100

    - add payments service web server config - add payments service jest test setup

---
+++ payments/src/index.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+import { natsClientWrapper } from './nats-client-wrapper';
+import { OrderCancelledListener } from './events/listener/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listener/order-created-listener';
+
+const start = async () => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET must be defined.');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined.');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined.');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined.');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined.');
+  }
+  
+  try {
+    await natsClientWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsClientWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsClientWrapper.client.close());
+    process.on('SIGINT', () => natsClientWrapper.client.close());
+
+    new OrderCreatedListener(natsClientWrapper.client).listen();
+    new OrderCancelledListener(natsClientWrapper.client).listen();
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('connected to mongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+  
+};
+app.listen(3000, () => {
+  console.log('Listening on Port 3000!!!'); 
+});
+
+start();
---
+++ payments/src/test/auth-helper.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { JwtManager } from '@saheedpass/common';
+
+export const getCookie = () => {
+  // create payload
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+    password: 'password'
+  };
+
+  // create JWT!
+  const token = JwtManager.generateToken(payload);
+
+  //  Encode JSON session object to base64
+  const base64 = Buffer.from(JSON.stringify({ jwt: token })).toString('base64');
+
+  return [`express:sess=${base64}`];
+};
