6c691ccfaae42cf8c79b2d9f9b81de08ae612dbc
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-07-02 22:23:33 +0100

    - add PaymentCreatedListener to Orders service - Listen for Payment Creation in Orders service and mark order as complete

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -5,6 +5,7 @@ import { natsClientWrapper } from './nats-client-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
   if (!process.env.JWT_SECRET) {
@@ -41,6 +42,7 @@ const start = async () => {
     new TicketCreatedListener(natsClientWrapper.client).listen();
     new TicketUpdatedListener(natsClientWrapper.client).listen();
     new ExpirationCompleteListener(natsClientWrapper.client).listen();
+    new PaymentCreatedListener(natsClientWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
